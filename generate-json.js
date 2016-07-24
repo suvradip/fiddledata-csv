9a8bf6c6cf32ddcecd2877446755b19ff9741fd4
suvradip <suvradip@fusioncharts.com>
2016-07-24 20:03:30 +0530

    - code formatting

--- generate-json.js
+++ generate-json.js
@@ -4,36 +4,31 @@
  */
 
 var fs,
+		inputFileName,
+		outputFileName,
 		csvFileData,
 		catid,
 		fiddleid,
  		getId,
  		ParentCategory,
- 		getParentCategory,
- 		setParentCategory,
- 		fiddleData,
- 		getFiddleData,
- 		setFiddleData,
- 		ChildCategory,
- 		getChildCategory,
- 		setChildCategory,
- 		fiddleToCategory,
- 		setFiddleToCategory,
- 		getVizDataFormat,
- 		generate;
+ 		FiddleData,
+ 		FiddleToCategory,
+ 		Operations;
 
 //fs module loading
 fs = require("fs");
 //variable defination
+inputFileName = "fiddleData-maping.csv";
+outputFileName = "demo.json";
 catid = 0;
 fiddleid = 0;
 ParentCategory = {};
 ChildCategory = {};
-fiddleData = {};
-XChild = {};
-fiddleToCategory = [];
+FiddleData = {};
+FiddleToCategory = {};
+Operations = {};
 //inmporting csv file	using fs module
-csvFileData = fs.readFileSync("fiddleData-maping.csv","utf-8").split("\n");
+csvFileData = fs.readFileSync(inputFileName,"utf-8").split("\n");
 
 /**
  * @description - generation id for categories and fiddles
@@ -50,134 +45,165 @@ getId = {
 	}
 };
 
+
+
 /**
- * @description - helps to find out the particular parent name from pool or parentCategory
- * @param {String} name - Name of the parent
- * @return {String} - Id of the parent  
+ * @description - helps to find out the particular parent name or set the data in ParentCategory object.
+ *
  */
-getParentCategory = (function(name){
-	return ParentCategory[name].cat_id;
-});
-
-setParentCategory = (function(obj){
-	var catName;
-	catName = obj.cat_name.toLowerCase();
-	if(!ParentCategory.hasOwnProperty(obj.cat_name)) {
-		obj.cat_id = getId.category(); //getId.parent();
-		ParentCategory[obj.cat_name] = obj;
-		return obj.cat_id;
-	} else {
-		return getParentCategory(obj.cat_name);
-	}
-});
-
-//return the fiddle-id
-getFiddleData = (function(code){
-	return fiddleData[code].fiddle_id;
-});
-
-setFiddleData = (function(obj){
-	var code,
-			furl;
-	furl = obj.fiddle_url;
-	
-	if(furl && typeof furl !== "undefined"){
-		code = furl.charAt(furl.length - 1) === "/" ? furl.split("/").join("") : furl.split("/").pop();
-		if(!fiddleData.hasOwnProperty(code)){
-			obj.fiddle_id = getId.fiddle();
-			fiddleData[code] = obj;
-			return obj.fiddle_id;
+ParentCategory = {
+	data : {},
+	get : function(name){
+		return ParentCategory.data[name].cat_id;
+	},
+	set : function(obj){
+		var catName;
+		catName = obj.cat_name.toLowerCase();
+		if(!ParentCategory.data.hasOwnProperty(obj.cat_name)) {
+			obj.cat_id = getId.category(); //getId.parent();
+			ParentCategory.data[obj.cat_name] = obj;
+			return obj.cat_id;
 		} else {
-			return getFiddleData(code);
+			return ParentCategory.get(obj.cat_name);
 		}
-	} else {
-		console.log("fiddle undefined");
-	}	
-});
-
-
-//return child category-id
-getChildCategory = (function(name){
-	return ChildCategory[name.toLowerCase()].cat_id;
-});
-
-setChildCategory = (function(obj){
-	var catName;
-	catName = obj.cat_name.toLowerCase();
-
-	if(!ChildCategory.hasOwnProperty(catName)){
-		obj.cat_id = getId.category();
-		ChildCategory[catName] = obj;
-		return obj.cat_id;
-	} else {
-		return getChildCategory(catName);
 	}
-});
+};
 
 
 
-getVizDataFormat = (function(obj){
-	var viz;
-	if(obj[1] === obj[2])
-		viz = obj[2];
-	else 
-		viz = obj[1]+","+obj[2];
+/**
+ * @description - helps to find out the particular childData informattion or set childData data in ChildCategory object.
+ * 
+ */
+ChildCategory = {
+	data : {},
+	get : function(name){
+		return ChildCategory.data[name.toLowerCase()].cat_id;
+	},
+	set : function(obj){
+		var catName;
+		catName = obj.cat_name.toLowerCase();
 
-	if(obj[3] && typeof obj[3] !== "undefined")
-		viz = viz+","+obj[3];
-
-	return viz;
-});
-
-setFiddleToCategory = (function(obj){
-	if(typeof obj.fiddle_id !== "undefined" && typeof obj.category_id !== "undefined") {
-		fiddleToCategory.push(obj);
+		if(!ChildCategory.data.hasOwnProperty(catName)){
+			obj.cat_id = getId.category();
+			ChildCategory.data[catName] = obj;
+			return obj.cat_id;
+		} else {
+			return ChildCategory.get(catName);
+		}
 	}
-});
+};
 
-generate = (function(){
-	var prepParent,
- 			prepChild,
- 			prepFiddle;
 
-	prepParent = [];
-	prepChild = [];
-	prepFiddle = [];
-	var output = {};
-	for(var pkey in ParentCategory) {
-		prepParent.push(ParentCategory[pkey]);
+
+/**
+ * @description - helps to find out the particular fiddle or set fiddle data in FiddleData object.
+ * 
+ */
+FiddleData = {
+	data : {},
+	get : function(code){
+		return FiddleData.data[code].fiddle_id;
+	},
+	set : function(obj){
+		var code,
+				furl;
+		furl = obj.fiddle_url;
+
+		if(furl && typeof furl !== "undefined"){
+			code = furl.charAt(furl.length - 1) === "/" ? furl.split("/").join("") : furl.split("/").pop();
+			if(!FiddleData.data.hasOwnProperty(code)){
+				obj.fiddle_id = getId.fiddle();
+				FiddleData.data[code] = obj;
+				return obj.fiddle_id;
+			} else {
+				return FiddleData.get(code);
+			}
+		} else {
+			//console.log("fiddle undefined");
+		}	
 	}
+};
 
-	for(var ckey in ChildCategory) {
-		prepChild.push(ChildCategory[ckey]);
+
+/**
+ * @description - Mapping each fiddle to each category
+ */
+FiddleToCategory = {
+	data : [],
+	set : function(obj){
+		if(typeof obj.fiddle_id !== "undefined" && typeof obj.category_id !== "undefined") {
+			FiddleToCategory.data.push(obj);
+		}
 	}
+};
 
-	for(var fkey in fiddleData) {
-		prepFiddle.push(fiddleData[fkey]);
+
+/**
+ * @description - Some internal operation and functionallity
+ */
+Operations = {
+	constructViz : function(obj){
+		var viz;
+		if(obj[1] === obj[2])
+			viz = obj[2];
+		else 
+			viz = obj[1]+","+obj[2];
+
+		if(obj[3] && typeof obj[3] !== "undefined")
+			viz = viz+","+obj[3];
+
+		return viz;
+	},
+	generate : function(){
+		var prepParent,
+	 			prepChild,
+	 			prepFiddle;
+
+		prepParent = [];
+		prepChild = [];
+		prepFiddle = [];
+		var output = {};
+		for(var pkey in ParentCategory.data) {
+			prepParent.push(ParentCategory.data[pkey]);
+		}
+
+		for(var ckey in ChildCategory.data) {
+			prepChild.push(ChildCategory.data[ckey]);
+		}
+
+		for(var fkey in FiddleData.data) {
+			prepFiddle.push(FiddleData.data[fkey]);
+		}
+	 
+	 output.ParentCategoryData = prepParent;
+	 output.ChildCategoryData = prepChild;
+	 output.FiddlesData = prepFiddle;
+	 output.FiddleToCategory = FiddleToCategory.data;
+	 
+	 fs.writeFileSync(outputFileName, JSON.stringify(output, null, 4));
+	},
+	run: function(){
+		for(var i=1; i<csvFileData.length; i++) {
+			var data,
+					fid,
+					cid,
+					pid;
+
+			data = csvFileData[i].split(",");	
+			pid = ParentCategory.set({cat_name:data[0]});
+			cid = ChildCategory.set({cat_name:data[2], parent_cat_id:pid, visualization_type: Operations.constructViz(data)});
+			fid = FiddleData.set({fiddle_url:data[4], fiddle_description:data[5], fiddle_thumb:data[6]});
+			
+			//data mapping
+			FiddleToCategory.set({fiddle_id:fid, category_id:ChildCategory.get(data[1])});
+			FiddleToCategory.set({fiddle_id:fid, category_id:cid});
+		}
 	}
- 
- output.ParentCategoryData = prepParent;
- output.ChildCategoryData = prepChild;
- output.FiddlesData = prepFiddle;
- output.FiddleToCategory= fiddleToCategory;
- 
- fs.writeFileSync("demo.json", JSON.stringify(output, null, 4));
-});
+};
 
-for(var i=1; i<csvFileData.length; i++) {
-	var data,
-			fid,
-			cid,
-			pid;
 
-	data = csvFileData[i].split(",");	
-	pid = setParentCategory({cat_name:data[0]});
-	cid = setChildCategory({cat_name:data[2], parent_cat_id:pid, visualization_type: getVizDataFormat(data)});
-	fid = setFiddleData({fiddle_url:data[4], fiddle_description:data[5], fiddle_thumb:data[6]});
-	
-	//data mapping
-	setFiddleToCategory({fiddle_id:fid, category_id:getChildCategory(data[1])});
-	setFiddleToCategory({fiddle_id:fid, category_id:cid});
-}
-
-generate();
+//running the program
+Operations.run();
+//exporting the output
+Operations.generate();
